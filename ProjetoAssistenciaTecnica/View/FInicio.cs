6ce27fe389eadcb037f264862d33cd458316cc17
Nogrilo <nogriloesirjeferson@gmail.com>
2025-08-28 20:13:15 -0300

    Criando pastas Controller, View e Model

--- ProjetoAssistenciaTecnica/View/FInicio.cs
+++ ProjetoAssistenciaTecnica/View/FInicio.cs
@@ -1,8 +1,8 @@
 namespace ProjetoAssistenciaTecnica
 {
-    public partial class Form1 : Form
+    public partial class FInicio : Form
     {
-        public Form1()
+        public FInicio()
         {
             InitializeComponent();
         }
