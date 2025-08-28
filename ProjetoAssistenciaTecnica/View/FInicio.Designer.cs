1b7254d2f15963a1fd495216745ff4c8757aabcb
Nogrilo <nogriloesirjeferson@gmail.com>
2025-08-28 20:23:08 -0300

    Adicionando o toolstrip para fazer a barra com os botoes

--- ProjetoAssistenciaTecnica/View/FInicio.Designer.cs
+++ ProjetoAssistenciaTecnica/View/FInicio.Designer.cs
@@ -28,21 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FInicio));
+            toolStrip1 = new ToolStrip();
+            botaoCadastrarCliente = new ToolStripButton();
+            toolStrip1.SuspendLayout();
             SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Items.AddRange(new ToolStripItem[] { botaoCadastrarCliente });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(1029, 25);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // botaoCadastrarCliente
+            // 
+            botaoCadastrarCliente.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            botaoCadastrarCliente.Image = (Image)resources.GetObject("botaoCadastrarCliente.Image");
+            botaoCadastrarCliente.ImageTransparentColor = Color.Magenta;
+            botaoCadastrarCliente.Name = "botaoCadastrarCliente";
+            botaoCadastrarCliente.Size = new Size(23, 22);
+            botaoCadastrarCliente.Text = "toolStripButton1";
+            botaoCadastrarCliente.ToolTipText = "Cadastrar Cliente";
             // 
             // FInicio
             // 
             AutoScaleDimensions = new SizeF(9F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1029, 535);
+            Controls.Add(toolStrip1);
             Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            Margin = new Padding(4, 4, 4, 4);
+            Margin = new Padding(4);
             Name = "FInicio";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Tela Inicial";
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ToolStrip toolStrip1;
+        private ToolStripButton botaoCadastrarCliente;
     }
 }
