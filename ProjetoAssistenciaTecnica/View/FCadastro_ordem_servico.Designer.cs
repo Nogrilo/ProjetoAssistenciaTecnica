7098e66def27cdf258b170753bc7a58307a527aa
ViniRanda <viniciuscorrea509@gmail.com>
2025-10-10 20:23:44 -0300

    Primeira edição forms Cadastro Ordem de Serviço

--- ProjetoAssistenciaTecnica/View/FCadastro_ordem_servico.Designer.cs
+++ ProjetoAssistenciaTecnica/View/FCadastro_ordem_servico.Designer.cs
@@ -46,117 +46,132 @@
             textBox1 = new TextBox();
             label9 = new Label();
             textBox8 = new TextBox();
+            label10 = new Label();
+            maskedTelefone = new MaskedTextBox();
+            txtBairro = new TextBox();
+            label11 = new Label();
+            txtRua = new TextBox();
+            label12 = new Label();
+            txtMunicipio = new TextBox();
+            label14 = new Label();
+            label18 = new Label();
+            txtNome = new TextBox();
+            label21 = new Label();
+            txtNumeroCasa = new TextBox();
+            label15 = new Label();
             SuspendLayout();
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(26, 103);
+            label2.Location = new Point(125, 416);
             label2.Name = "label2";
-            label2.Size = new Size(61, 20);
+            label2.Size = new Size(64, 20);
             label2.TabIndex = 0;
-            label2.Text = "modelo";
+            label2.Text = "Modelo:";
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(114, 103);
+            textBox2.Location = new Point(221, 413);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(125, 27);
+            textBox2.Size = new Size(179, 27);
             textBox2.TabIndex = 1;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(26, 143);
+            label3.Location = new Point(125, 459);
             label3.Name = "label3";
-            label3.Size = new Size(50, 20);
+            label3.Size = new Size(53, 20);
             label3.TabIndex = 0;
-            label3.Text = "marca";
+            label3.Text = "Marca:";
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(114, 143);
+            textBox3.Location = new Point(221, 456);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(125, 27);
+            textBox3.Size = new Size(179, 27);
             textBox3.TabIndex = 1;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(26, 354);
+            label4.Location = new Point(125, 501);
             label4.Name = "label4";
-            label4.Size = new Size(77, 20);
+            label4.Size = new Size(82, 20);
             label4.TabIndex = 0;
-            label4.Text = "acessorios";
+            label4.Text = "Acessórios:";
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(114, 354);
+            textBox4.Location = new Point(221, 498);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(458, 27);
+            textBox4.Size = new Size(179, 27);
             textBox4.TabIndex = 1;
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(26, 176);
+            label5.Location = new Point(430, 416);
             label5.Name = "label5";
-            label5.Size = new Size(52, 20);
+            label5.Size = new Size(64, 20);
             label5.TabIndex = 0;
-            label5.Text = "n serie";
+            label5.Text = "N° série:";
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(114, 176);
+            textBox5.Location = new Point(511, 413);
             textBox5.Name = "textBox5";
-            textBox5.Size = new Size(125, 27);
+            textBox5.Size = new Size(151, 27);
             textBox5.TabIndex = 1;
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(26, 229);
+            label6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(37, 94);
             label6.Name = "label6";
-            label6.Size = new Size(112, 20);
+            label6.Size = new Size(82, 40);
             label6.TabIndex = 0;
-            label6.Text = "cpf/cnpj cliente";
+            label6.Text = "CPF/CNPJ \r\nCliente:";
             // 
             // textBox6
             // 
-            textBox6.Location = new Point(144, 222);
+            textBox6.Location = new Point(125, 102);
             textBox6.Name = "textBox6";
-            textBox6.Size = new Size(125, 27);
+            textBox6.Size = new Size(239, 27);
             textBox6.TabIndex = 1;
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(26, 262);
+            label7.Location = new Point(125, 550);
             label7.Name = "label7";
-            label7.Size = new Size(91, 20);
+            label7.Size = new Size(96, 20);
             label7.TabIndex = 0;
-            label7.Text = "observações";
+            label7.Text = "Observações:";
             // 
             // textBox7
             // 
-            textBox7.Location = new Point(123, 262);
+            textBox7.Location = new Point(125, 573);
+            textBox7.Multiline = true;
             textBox7.Name = "textBox7";
-            textBox7.Size = new Size(444, 27);
+            textBox7.Size = new Size(578, 58);
             textBox7.TabIndex = 1;
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(25, 306);
+            label8.Location = new Point(430, 459);
             label8.Name = "label8";
-            label8.Size = new Size(70, 20);
+            label8.Size = new Size(75, 20);
             label8.TabIndex = 0;
-            label8.Text = "condição";
+            label8.Text = "Condição:";
             // 
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(101, 306);
+            comboBox1.Location = new Point(511, 456);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(151, 28);
             comboBox1.TabIndex = 2;
@@ -164,40 +179,170 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(26, 24);
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(639, 26);
             label1.Name = "label1";
-            label1.Size = new Size(136, 20);
+            label1.Size = new Size(83, 20);
             label1.TabIndex = 0;
-            label1.Text = "n ordem de serviço";
+            label1.Text = "N° Ordem:";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(168, 21);
+            textBox1.Location = new Point(724, 23);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(125, 27);
+            textBox1.Size = new Size(62, 27);
             textBox1.TabIndex = 1;
             // 
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(26, 70);
+            label9.Location = new Point(123, 299);
             label9.Name = "label9";
-            label9.Size = new Size(57, 20);
+            label9.Size = new Size(129, 20);
             label9.TabIndex = 0;
-            label9.Text = "defeito";
+            label9.Text = "Descrição defeito:";
             // 
             // textBox8
             // 
-            textBox8.Location = new Point(101, 67);
+            textBox8.Location = new Point(125, 322);
+            textBox8.Multiline = true;
             textBox8.Name = "textBox8";
-            textBox8.Size = new Size(466, 27);
+            textBox8.Size = new Size(578, 78);
             textBox8.TabIndex = 1;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.Location = new Point(12, 9);
+            label10.Name = "label10";
+            label10.Size = new Size(282, 41);
+            label10.TabIndex = 3;
+            label10.Text = "Ordem de Serviços";
+            // 
+            // maskedTelefone
+            // 
+            maskedTelefone.Location = new Point(596, 160);
+            maskedTelefone.Mask = "(##) # ####-####";
+            maskedTelefone.Name = "maskedTelefone";
+            maskedTelefone.Size = new Size(190, 27);
+            maskedTelefone.TabIndex = 54;
+            // 
+            // txtBairro
+            // 
+            txtBairro.Location = new Point(125, 240);
+            txtBairro.Name = "txtBairro";
+            txtBairro.Size = new Size(360, 27);
+            txtBairro.TabIndex = 63;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(37, 243);
+            label11.Margin = new Padding(4, 0, 4, 0);
+            label11.Name = "label11";
+            label11.Size = new Size(52, 20);
+            label11.TabIndex = 71;
+            label11.Text = "Bairro:";
+            // 
+            // txtRua
+            // 
+            txtRua.Location = new Point(125, 201);
+            txtRua.Name = "txtRua";
+            txtRua.Size = new Size(360, 27);
+            txtRua.TabIndex = 62;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(37, 201);
+            label12.Margin = new Padding(4, 0, 4, 0);
+            label12.Name = "label12";
+            label12.Size = new Size(37, 20);
+            label12.TabIndex = 70;
+            label12.Text = "Rua:";
+            // 
+            // txtMunicipio
+            // 
+            txtMunicipio.Location = new Point(596, 204);
+            txtMunicipio.Name = "txtMunicipio";
+            txtMunicipio.Size = new Size(190, 27);
+            txtMunicipio.TabIndex = 61;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(508, 204);
+            label14.Margin = new Padding(4, 0, 4, 0);
+            label14.Name = "label14";
+            label14.Size = new Size(78, 20);
+            label14.TabIndex = 69;
+            label14.Text = "Municipio:";
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.Location = new Point(37, 159);
+            label18.Margin = new Padding(4, 0, 4, 0);
+            label18.Name = "label18";
+            label18.Size = new Size(53, 20);
+            label18.TabIndex = 52;
+            label18.Text = "Nome:";
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(125, 156);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(360, 27);
+            txtNome.TabIndex = 51;
+            // 
+            // label21
+            // 
+            label21.AutoSize = true;
+            label21.Location = new Point(508, 163);
+            label21.Margin = new Padding(4, 0, 4, 0);
+            label21.Name = "label21";
+            label21.Size = new Size(69, 20);
+            label21.TabIndex = 58;
+            label21.Text = "Telefone:";
+            // 
+            // txtNumeroCasa
+            // 
+            txtNumeroCasa.Location = new Point(596, 243);
+            txtNumeroCasa.Name = "txtNumeroCasa";
+            txtNumeroCasa.Size = new Size(92, 27);
+            txtNumeroCasa.TabIndex = 65;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Font = new Font("Segoe UI", 9F);
+            label15.ForeColor = SystemColors.ControlText;
+            label15.Location = new Point(508, 246);
+            label15.Margin = new Padding(4, 0, 4, 0);
+            label15.Name = "label15";
+            label15.Size = new Size(29, 20);
+            label15.TabIndex = 73;
+            label15.Text = "Nº:";
             // 
             // FCadastro_ordem_servico
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(851, 674);
+            Controls.Add(maskedTelefone);
+            Controls.Add(txtNumeroCasa);
+            Controls.Add(label15);
+            Controls.Add(txtBairro);
+            Controls.Add(label11);
+            Controls.Add(txtRua);
+            Controls.Add(label12);
+            Controls.Add(txtMunicipio);
+            Controls.Add(label14);
+            Controls.Add(label18);
+            Controls.Add(txtNome);
+            Controls.Add(label21);
+            Controls.Add(label10);
             Controls.Add(comboBox1);
             Controls.Add(textBox7);
             Controls.Add(textBox6);
@@ -241,5 +386,20 @@
         private TextBox textBox1;
         private Label label9;
         private TextBox textBox8;
+        private Label label10;
+        private ComboBox comboComplemento;
+        private MaskedTextBox maskedTelefone;
+        private TextBox txtNumeroCasa;
+        private Label label15;
+        private Label label13;
+        private TextBox txtBairro;
+        private Label label11;
+        private TextBox txtRua;
+        private Label label12;
+        private TextBox txtMunicipio;
+        private Label label14;
+        private Label label18;
+        private TextBox txtNome;
+        private Label label21;
     }
 }
