de5400d2cc12e76838fadb596e9c6d2d3e50a485
Nogrilo <nogriloesirjeferson@gmail.com>
2025-10-10 21:14:55 -0300

    Adicionando o botao de cadastrar ordem de servico

--- ProjetoAssistenciaTecnica/View/FInicio.Designer.cs
+++ ProjetoAssistenciaTecnica/View/FInicio.Designer.cs
@@ -32,9 +32,9 @@
             toolStrip1 = new ToolStrip();
             botaoCadastrarCliente = new ToolStripButton();
             botaoCadastrarFuncionario = new ToolStripButton();
+            botaoOrdemDeServico = new ToolStripButton();
             panel1 = new Panel();
             tabControl1 = new TabControl();
-            toolStripButton1 = new ToolStripButton();
             toolStrip1.SuspendLayout();
             panel1.SuspendLayout();
             SuspendLayout();
@@ -45,7 +45,7 @@
             toolStrip1.Dock = DockStyle.Left;
             toolStrip1.GripMargin = new Padding(10, 5, 10, 5);
             toolStrip1.ImageScalingSize = new Size(40, 40);
-            toolStrip1.Items.AddRange(new ToolStripItem[] { botaoCadastrarCliente, botaoCadastrarFuncionario, toolStripButton1 });
+            toolStrip1.Items.AddRange(new ToolStripItem[] { botaoCadastrarCliente, botaoCadastrarFuncionario, botaoOrdemDeServico });
             toolStrip1.Location = new Point(0, 0);
             toolStrip1.Name = "toolStrip1";
             toolStrip1.RenderMode = ToolStripRenderMode.Professional;
@@ -74,6 +74,16 @@
             botaoCadastrarFuncionario.Text = "Funcionarios";
             botaoCadastrarFuncionario.Click += botaoCadastrarFuncionario_Click;
             // 
+            // botaoOrdemDeServico
+            // 
+            botaoOrdemDeServico.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            botaoOrdemDeServico.Image = (Image)resources.GetObject("botaoOrdemDeServico.Image");
+            botaoOrdemDeServico.ImageTransparentColor = Color.Magenta;
+            botaoOrdemDeServico.Name = "botaoOrdemDeServico";
+            botaoOrdemDeServico.Size = new Size(42, 44);
+            botaoOrdemDeServico.Text = "toolStripButton1";
+            botaoOrdemDeServico.Click += botaoOrdemDeServico_Click;
+            // 
             // panel1
             // 
             panel1.Controls.Add(tabControl1);
@@ -92,19 +102,9 @@
             tabControl1.Size = new Size(984, 535);
             tabControl1.TabIndex = 0;
             // 
-            // toolStripButton1
-            // 
-            toolStripButton1.DisplayStyle = ToolStripItemDisplayStyle.Image;
-            toolStripButton1.Image = (Image)resources.GetObject("toolStripButton1.Image");
-            toolStripButton1.ImageTransparentColor = Color.Magenta;
-            toolStripButton1.Name = "toolStripButton1";
-            toolStripButton1.Size = new Size(42, 44);
-            toolStripButton1.Text = "toolStripButton1";
-            toolStripButton1.Click += toolStripButton1_Click;
-            // 
             // FInicio
             // 
-            AutoScaleDimensions = new SizeF(11F, 28F);
+            AutoScaleDimensions = new SizeF(9F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoSize = true;
             ClientSize = new Size(1029, 535);
@@ -116,6 +116,7 @@
             Name = "FInicio";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Tela Inicial";
+            WindowState = FormWindowState.Maximized;
             toolStrip1.ResumeLayout(false);
             toolStrip1.PerformLayout();
             panel1.ResumeLayout(false);
@@ -130,6 +131,6 @@
         private Panel panel1;
         private TabControl tabControl1;
         private ToolStripButton botaoCadastrarFuncionario;
-        private ToolStripButton toolStripButton1;
+        private ToolStripButton botaoOrdemDeServico;
     }
 }
--- ProjetoAssistenciaTecnica/View/FInicio.cs
+++ ProjetoAssistenciaTecnica/View/FInicio.cs
@@ -74,9 +74,36 @@ namespace ProjetoAssistenciaTecnica
             tabControl1.TabPages[abas - 1].ImageIndex = 0;
         }
 
-        private void toolStripButton1_Click(object sender, EventArgs e)
+        private void botaoOrdemDeServico_Click(object sender, EventArgs e)
         {
+            /* Abrindo a tela de cadastro de ordem de servico */
 
+            FCadastro_ordem_servico tela = new FCadastro_ordem_servico();
+
+            tela.TopLevel = false;
+            tela.FormBorderStyle = FormBorderStyle.None;
+
+            /* Adicionar uma nova aba ja existente */
+            tabControl1.TabPages.Add("Ordem de Servico");
+
+            /* Identificar quantas abas ja existem abertas */
+            int abas = tabControl1.TabPages.Count;
+
+            /* Adicionar o formulario na ultima aba */
+            tabControl1.TabPages[abas - 1].Controls.Add(tela);
+
+            /* Redimensionar ara o tamanho total da aba */
+            tela.Dock = DockStyle.Fill;
+
+            /* Mostrar Janela */
+            tela.Show();
+            Refresh();
+
+            /* Selecionar a aba recem criada */
+            tabControl1.SelectedTab = tabControl1.TabPages[abas - 1];
+
+            /* Adicionar um icone na aba*/
+            tabControl1.TabPages[abas - 1].ImageIndex = 0;
         }
     }
 }
