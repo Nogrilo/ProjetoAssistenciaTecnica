6268e1a370e293686296f7f96ec88ee3c1655016
Nogrilo <nogriloesirjeferson@gmail.com>
2025-11-09 16:31:09 -0300

    adicionando a classe "Peca" para fazer o cadastro de pecas

---
+++ ProjetoAssistenciaTecnica/Model/Peca.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAssistenciaTecnica.Model
+{
+    public class Peca
+    {
+        public int idPeca { get; set; } = 0;
+
+        public string tipo { get; set; } = string.Empty;
+
+        public float valorCusto { get; set; } = 0;
+
+        public float valorRepasse {  get; set; } = 0;
+        
+        public float codigo { get; set; } = 0;
+    }
+}
