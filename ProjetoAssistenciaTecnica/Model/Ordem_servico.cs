4c71a4aa35a34d363aa52e7f1b287004a78967ac
Nogrilo <nogriloesirjeferson@gmail.com>
2025-11-25 21:05:20 -0300

    criando a classe de ordem_servico

---
+++ ProjetoAssistenciaTecnica/Model/Ordem_servico.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAssistenciaTecnica.Model
+{
+    public class Ordem_servico
+    {
+        public int id_ordem_servico { get; set; } = 0;
+
+        public int tick { get; set; } = 0;
+
+        public int n_atendimento {  get; set; } = 0;
+
+        public string defeito { get; set; } = string.Empty;
+
+        public DateTime data_abertura { get; set; }
+
+        public string acessorios { get; set; } = string.Empty;
+
+        public string status { get; set; } = string.Empty;
+
+        public int id_orcamento_inicial { get; set; } = 0;
+
+        public int id_pertencente { get; set; } = 0;
+
+        public int id_produto { get; set; } = 0;
+    }
+}
